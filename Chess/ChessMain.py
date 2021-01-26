079664cb1681aca5acaaaf7862aa67d17f7098e9
Dmitry <53148485+cornet-dmitry@users.noreply.github.com>
2021-01-26 10:51:18 +0300

    Added selection of field cells. Added animation.

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -52,7 +52,6 @@ def main():
                     playerClicks.append(sqSelected)
                 if len(playerClicks) == 2:  # after 2nd click
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
-                    print(move.getChessNotation())
                     if move in validMoves:
                         gs.makeMove(move)
                         moveMade = True
@@ -67,20 +66,41 @@ def main():
                     moveMade = True
 
         if moveMade:
+            animateMove(gs.moveLog[-1], screen, gs.board, clock)
             validMoves = gs.getValidMoves()
             moveMade = False
 
-        drawGameState(screen, gs)
+        drawGameState(screen, gs, validMoves, sqSelected)
         clock.tick(MAX_FPS)
         pg.display.flip()
 
 
-def drawGameState(screen, gs):
+"""Highlight square selected and moves for piece selected"""
+
+
+def highlightSquares(screen, gs, validMoves, sqSelected):
+    if sqSelected != ():
+        r, c = sqSelected
+        if gs.board[r][c][0] == ('w' if gs.whiteToMove else 'b'):
+            s = pg.Surface((SQ_SIZE, SQ_SIZE))
+            s.set_alpha(100)
+            s.fill(pg.Color('blue'))
+            screen.blit(s, (c * SQ_SIZE, r * SQ_SIZE))
+            s.fill(pg.Color('yellow'))
+
+            for move in validMoves:
+                if move.startRow == r and move.startCol == c:
+                    screen.blit(s, (move.endCol * SQ_SIZE, move.endRow * SQ_SIZE))
+
+
+def drawGameState(screen, gs, validMoves, sqSelected):
     drawBoard(screen)  # draw squares on the board
     drawPieces(screen, gs.board)  # draw pieces on top of those squares
+    highlightSquares(screen, gs, validMoves, sqSelected)
 
 
 def drawBoard(screen):
+    global colors
     # colors = [pg.Color('white'), pg.Color('gray')]
     colors = [(242, 216, 183), (181, 136, 100)]
     for row in range(DIMENSION):
@@ -95,6 +115,32 @@ def drawPieces(screen, board):
             piece = board[row][col]
             if piece != '--':
                 screen.blit(IMAGES[piece], pg.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+"""Animating a move"""
+
+
+def animateMove(move, screen, board, clock):
+    global colors
+    coords = []
+    dR = move.endRow - move.startRow
+    dC = move.endCol - move.startCol
+    framesPerSquare = 10  # frames to move one square
+    framesCount = (abs(dR) + abs(dC)) * framesPerSquare
+
+    for frame in range(framesCount + 1):
+        r, c = (move.startRow + dR * frame / framesCount, move.startCol + dC * frame / framesCount)
+        drawBoard(screen)
+        drawPieces(screen, board)
+        color = colors[(move.endRow + move.endCol) % 2]
+        endSquare = pg.Rect(move.endCol * SQ_SIZE, move.endRow * SQ_SIZE, SQ_SIZE, SQ_SIZE)
+        pg.draw.rect(screen, color, endSquare)
+
+        if move.pieceCaptured != '--':
+            screen.blit(IMAGES[move.pieceCaptured], endSquare)
+        screen.blit(IMAGES[move.pieceMoved], pg.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+        pg.display.flip()
+        clock.tick(120)
 
 
 if __name__ == '__main__':
